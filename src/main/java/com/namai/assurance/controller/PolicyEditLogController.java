d9c3d9a2f47d702ea6cc9f147e63937a028ecdaa
Lucas <61554619+Heyimlulu@users.noreply.github.com>
2021-03-05 10:16:47 +0100

    Add PaymentDetail table (Backend)

--- src/main/java/com/namai/assurance/controller/PolicyEditLogController.java
+++ src/main/java/com/namai/assurance/controller/PolicyEditLogController.java
@@ -35,10 +35,10 @@ public class PolicyEditLogController {
 			policyEditLog.setPolicy(p);
 			PolicyEditLog returnedPolicy = policyEditLogServices.savePolicyEditLog(policyEditLog);
 
-			return new ResponseEntity<Message>(new Message("Upload Successfully!", Arrays.asList(returnedPolicy), null, ""),
+			return new ResponseEntity<Message>(new Message("Upload Successfully!", Arrays.asList(returnedPolicy), null, null, ""),
 					HttpStatus.OK);
 		} catch (Exception e) {
-			return new ResponseEntity<Message>(new Message("Fail to post a new Policy!", null, null, e.getMessage()),
+			return new ResponseEntity<Message>(new Message("Fail to post a new Policy!", null, null, null, e.getMessage()),
 					HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
@@ -50,9 +50,9 @@ public class PolicyEditLogController {
 			List<PolicyEditLog> policyEditLogInfos = policyEditLogServices.getPolicyEditLogInfos();
 
 			return new ResponseEntity<Message>(new Message("Get Policys Edit Log' Infos!", 
-					policyEditLogInfos, null, ""), HttpStatus.OK);
+					policyEditLogInfos, null, null, ""), HttpStatus.OK);
 		} catch (Exception e) {
-			return new ResponseEntity<Message>(new Message("Fail!", null, null, e.getMessage()),
+			return new ResponseEntity<Message>(new Message("Fail!", null, null, null, e.getMessage()),
 					HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
@@ -64,13 +64,13 @@ public class PolicyEditLogController {
 
 			if (optPolicy.isPresent()) {
 				return new ResponseEntity<Message>(new Message("Successfully! Retrieve a policy by id = " + id,
-						Arrays.asList(optPolicy.get()), null, ""), HttpStatus.OK);
+						Arrays.asList(optPolicy.get()), null, null, ""), HttpStatus.OK);
 			} else {
-				return new ResponseEntity<Message>(new Message("Failure -> NOT Found a policy by id = " + id, null, null, ""),
+				return new ResponseEntity<Message>(new Message("Failure -> NOT Found a policy by id = " + id, null, null, null, ""),
 						HttpStatus.NOT_FOUND);
 			}
 		} catch (Exception e) {
-			return new ResponseEntity<Message>(new Message("Failure", null, null, e.getMessage()),
+			return new ResponseEntity<Message>(new Message("Failure", null, null, null, e.getMessage()),
 					HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
@@ -91,14 +91,14 @@ public class PolicyEditLogController {
 				policyEditLogServices.updatePolicyEditLog(policyEditLog);
 
 				return new ResponseEntity<Message>(
-						new Message("Successfully! Updated a PolicyEditLog " + "with id = " + id, null, null, ""), null);
+						new Message("Successfully! Updated a PolicyEditLog " + "with id = " + id, null, null, null, ""), null);
 			} else {
 				return new ResponseEntity<Message>(
-						new Message("Failer! Can NOT Found a Policy " + "with id = " + id, null, null, ""),
+						new Message("Failer! Can NOT Found a Policy " + "with id = " + id, null, null, null, ""),
 						HttpStatus.NOT_FOUND);
 			}
 		} catch (Exception e) {
-			return new ResponseEntity<Message>(new Message("Failure", null, null, e.getMessage()),
+			return new ResponseEntity<Message>(new Message("Failure", null, null, null, e.getMessage()),
 					HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
@@ -111,14 +111,14 @@ public class PolicyEditLogController {
 				policyEditLogServices.deletePolicyEditLogById(id);
 
 				return new ResponseEntity<Message>(
-						new Message("Successfully! Delete a PolicyEditLog with id = " + id, null, null, ""), HttpStatus.OK);
+						new Message("Successfully! Delete a PolicyEditLog with id = " + id, null, null, null, ""), HttpStatus.OK);
 			} else {
 				return new ResponseEntity<Message>(
-						new Message("Failer! Can NOT Found a PolicyEditLog " + "with id = " + id, null, null, ""),
+						new Message("Failer! Can NOT Found a PolicyEditLog " + "with id = " + id, null, null, null, ""),
 						HttpStatus.NOT_FOUND);
 			}
 		} catch (Exception e) {
-			return new ResponseEntity<Message>(new Message("Failure", null, null, e.getMessage()),
+			return new ResponseEntity<Message>(new Message("Failure", null, null, null, e.getMessage()),
 					HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
--- src/main/java/com/namai/assurance/model/Message.java
+++ src/main/java/com/namai/assurance/model/Message.java
@@ -8,6 +8,7 @@ public class Message {
 	private List<Policy> policies = new ArrayList<Policy>();
 	private List<PolicyEditLog> editLogPolicies = new ArrayList<PolicyEditLog>();
 	private List<Bill> bill = new ArrayList<Bill>();
+	private List<PaymentDetail> paymentDetail = new ArrayList<PaymentDetail>();
 	private String error = "";
 	
 	public String getMessage() { return this.message; }
@@ -28,19 +29,25 @@ public class Message {
 	public List<Bill> getBill() { return bill; }
 	public void setBill(List<Bill> bill) { this.bill = bill; }
 	
+	public List<PaymentDetail> getPaymentDetail() { return paymentDetail; }
+	public void setPaymentDetail(List<PaymentDetail> paymentDetail) { this.paymentDetail = paymentDetail; }
+	
 
-	public Message(String message, List<Policy> policies, List<PolicyEditLog> editLogPolicies, List<Bill> bill, String error) {
+	public Message(String message, List<Policy> policies, List<PolicyEditLog> editLogPolicies, List<Bill> bill, List<PaymentDetail> paymentDetail, String error) {
 		this.message = message;
 		this.policies = policies;
 		this.editLogPolicies = editLogPolicies;
 		this.bill = bill;
+		this.paymentDetail = paymentDetail;
 		this.error = error;
 	}
+
 	
-	public Message(String message, List<PolicyEditLog> editLogPolicies, List<Bill> bill, String error) {
+	public Message(String message, List<PolicyEditLog> editLogPolicies, List<Bill> bill, List<PaymentDetail> paymentDetail, String error) {
 		this.message = message;
 		this.editLogPolicies = editLogPolicies;
 		this.bill = bill;
+		this.paymentDetail = paymentDetail;
 		this.error = error;
 	}
 }
