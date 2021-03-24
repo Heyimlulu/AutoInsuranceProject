5c29921514fdf87a6505a30ddcec96aa275b04f2
Lucas Rouiller-Monay Admin <roluadm@aigle.sectioninformatique.ch>
2021-03-24 13:12:44 +0100

    Add vehicle table (Back-End)

---
+++ src/main/java/com/namai/assurance/controller/VehicleController.java
@@ -0,0 +1,114 @@
+package com.namai.assurance.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.namai.assurance.model.Policy;
+import com.namai.assurance.model.Message;
+import com.namai.assurance.model.Vehicle;
+import com.namai.assurance.service.VehicleServices;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/vehicle")
+public class VehicleController {
+
+	@Autowired
+	VehicleServices vehicleServices;
+
+	@PostMapping("/create/{id}")
+	public ResponseEntity<Message> addNewVehicle(@RequestBody Vehicle vehicle, @PathVariable long id) {
+		try {
+			Policy p = vehicleServices.getPolicyServices().getPolicyById(id).get();
+			vehicle.setPolicy(p);
+			Vehicle returnedVehicle = vehicleServices.saveVehicle(vehicle);
+
+			return new ResponseEntity<Message>(new Message("Upload Successfully!", null, null, null, null, Arrays.asList(returnedVehicle), ""), HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<Message>(new Message("Fail to post a new Vehicle!", null, null, null, null, e.getMessage()),	HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+	
+	@GetMapping("/retrieveinfos")
+	public ResponseEntity<Message> retrieveVehicleInfo() {
+
+		try {
+			List<Vehicle> paymentDetailInfos = vehicleServices.getVehicleInfos();
+
+			return new ResponseEntity<Message>(new Message("Get Vehicle's Infos!", null, null, null, null, paymentDetailInfos, ""), HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<Message>(new Message("Fail!", null, null, null, null, e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	@GetMapping("/findone/{id}")
+	public ResponseEntity<Message> getVehicleById(@PathVariable long id) {
+		try {
+			Optional<Vehicle> optBill = vehicleServices.getVehicleById(id);
+
+			if (optBill.isPresent()) {
+				return new ResponseEntity<Message>(new Message("Successfully! Retrieve a Vehicle by id = " + id, null, null, null, Arrays.asList(optBill.get()), ""), HttpStatus.OK);
+			} else {
+				return new ResponseEntity<Message>(new Message("Failure -> NOT Found a Vehicle by id = " + id, null, null, null, null, ""), HttpStatus.NOT_FOUND);
+			}
+		} catch (Exception e) {
+			return new ResponseEntity<Message>(new Message("Failure", null, null, null, null, e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	@PutMapping("/updatebyid/{id}")
+	public ResponseEntity<Message> updateVehicleById(@RequestBody Vehicle _vehicle, @PathVariable long id) {
+		try {
+			if (vehicleServices.checkExistedVehicle(id)) {
+				Vehicle vehicle = vehicleServices.getVehicleById(id).get();
+
+				// set new values for Vehicle
+				vehicle.setYear(_vehicle.getYear());
+				vehicle.setMake(_vehicle.getMake());
+				vehicle.setModel(_vehicle.getModel());
+				vehicle.setColor(_vehicle.getColor());
+				vehicle.setTrim(_vehicle.getTrim());
+				vehicle.setMileAge(_vehicle.getMileAge());
+				vehicle.setVinNumber(_vehicle.getVinNumber());
+				vehicle.setVehicleNumberPlate(_vehicle.getVehicleNumberPlate());
+				vehicle.setVehicleRegisteredState(_vehicle.getVehicleRegisteredState());
+				vehicle.setCreatedDate(_vehicle.getCreatedDate());
+				vehicle.setActive(_vehicle.getActive());
+
+				// save the change to database
+				vehicleServices.updateVehicle(vehicle);
+
+				return new ResponseEntity<Message>(
+						new Message("Successfully! Updated a Vehicle " + "with id = " + id, null, null, null, null, ""), HttpStatus.OK);
+			} else {
+				return new ResponseEntity<Message>(
+						new Message("Failer! Can NOT Found a Vehicle " + "with id = " + id, null, null, null, null, ""), HttpStatus.NOT_FOUND);
+			}
+		} catch (Exception e) {
+			return new ResponseEntity<Message>(new Message("Failure", null, null, null, null, e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	@DeleteMapping("/deletebyid/{id}")
+	public ResponseEntity<Message> deleteVehicleById(@PathVariable long id) {
+		try {
+			// checking the existed of a Policy with id
+			if (vehicleServices.checkExistedVehicle(id)) {
+				vehicleServices.deleteVehicleById(id);
+
+				return new ResponseEntity<Message>(
+						new Message("Successfully! Delete a Vehicle with id = " + id, null, null, null, null, ""), HttpStatus.OK);
+			} else {
+				return new ResponseEntity<Message>(
+						new Message("Failer! Can NOT Found a Vehicle " + "with id = " + id, null, null, null, null, ""), HttpStatus.NOT_FOUND);
+			}
+		} catch (Exception e) {
+			return new ResponseEntity<Message>(new Message("Failure", null, null, null, null, e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+}
---
+++ src/main/java/com/namai/assurance/model/Vehicle.java
@@ -0,0 +1,129 @@
+package com.namai.assurance.model;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import java.sql.Date;
+
+@Entity
+@Table(name = "paymentdetail")
+public class Vehicle {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+
+	@Column
+	private long year;
+	
+	@Column
+	private String make; // = Vehicle's Brand
+	
+	@Column
+	private String model;
+	
+	@Column
+	private String color;
+	
+	@Column
+	private String trim;
+	
+	@Column
+	private double mileAge;
+	
+	@Column
+	private long vinNumber;
+	
+	@Column
+	private long vehicleNumberPlate;
+	
+	@Column
+	private String vehicleRegisteredState;
+	
+	@Column
+	private Date createdDate;
+	
+	@Column
+	private boolean active;
+
+	@JsonBackReference
+	@ManyToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "policy_id")
+	private Policy policy;
+
+	public Policy getPolicy() { return policy; }
+	public void setPolicy(Policy policy) { this.policy = policy; }
+
+	public Vehicle() {}
+
+	public Vehicle(long id, long year, String make, String model, String color, String trim, double mileAge, 
+			long vinNumber, long vehicleNumberPlate, String vehicleRegisteredState, Date createdDate, boolean active, Policy policy) {
+		super();
+		this.id = id;
+		this.year = year;
+		this.make = make;
+		this.model = model;
+		this.color = color;
+		this.trim = trim;
+		this.mileAge = mileAge;
+		this.vinNumber = vinNumber;
+		this.vehicleNumberPlate = vehicleNumberPlate;
+		this.vehicleRegisteredState = vehicleRegisteredState;
+		this.createdDate = createdDate;
+		this.active = active;
+		
+		this.policy = policy;
+	}
+
+	public long getId() { return this.id; }
+	public void setId(long id) { this.id = id; }
+
+	public long getYear() { return year; }
+	public void setYear(long year) { this.year = year; }
+	
+	public String getMake() { return make; }
+	public void setMake(String make) { this.make = make; }
+	
+	public String getModel() { return model; }
+	public void setModel(String model) { this.model = model; }
+	
+	public String getColor() { return color; }
+	public void setColor(String color) { this.color = color; }
+	
+	public String getTrim() { return trim; }
+	public void setTrim(String trim) { this.trim = trim; }
+	
+	public double getMileAge() { return mileAge; }
+	public void setMileAge(double mileAge) { this.mileAge = mileAge; }
+	
+	public long getVinNumber() { return vinNumber; }
+	public void setVinNumber(long vinNumber) { this.vinNumber = vinNumber; }
+	
+	public long getVehicleNumberPlate() { return vehicleNumberPlate; }
+	public void setVehicleNumberPlate(long vehicleNumberPlate) { this.vehicleNumberPlate = vehicleNumberPlate; }
+	
+	public String getVehicleRegisteredState() { return vehicleRegisteredState; }
+	public void setVehicleRegisteredState(String vehicleRegisteredState) { this.vehicleRegisteredState = vehicleRegisteredState; }
+	
+	public Date getCreatedDate() { return createdDate; }
+	public void setCreatedDate(Date createdDate) { this.createdDate = createdDate; }
+	
+	public boolean getActive() { return active; }
+	public void setActive(boolean active) { this.active = active; }
+
+
+	@Override
+	public String toString() {
+		return "Vehicle [id=" + id + ", Year=" + year	+ ", Make=" + make + ", Model=" + model + ", Color=" + model + ", Trim=" + trim +
+				", Mile Age=" + mileAge + ", VINNumber=" + vinNumber + ", Vehicle Number Plate=" + vehicleNumberPlate + 
+				", Vehicle Registered State" + vehicleRegisteredState + ", Created Date=" + createdDate + ", Active=" + active + "]";
+	}
+}
